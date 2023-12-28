f2d56dfb7b7f05d595ffc1059100c2a68c3f33ae
polaris98 <proietti.1808759@studenti.uniroma1.it>
2023-12-28 13:22:11 +0100

    add yolo hand detection

--- Demo/demo.py
+++ Demo/demo.py
@@ -1,18 +1,15 @@
 import streamlit as st
-from streamlit_image_select import image_select
 import numpy as np
 from PIL import Image
+import cv2
+
+import mediapipe
 
 st.title("Sign Language Recognition Demo")
 st.text("Rube Rube Rube")
 
-img = image_select(
-    label="Select a cat",
-    images=[
-        "images/demarsico.jpg",
-        "https://bagongkia.github.io/react-image-picker/0759b6e526e3c6d72569894e58329d89.jpg",
-        Image.open("images/a1.jpg"),
-        np.array(Image.open("images/a1.jpg")),
-    ],
-    captions=["A cat", "Another cat", "Oh look, a cat!", "Guess what, a cat..."],
-)
+picture = st.camera_input("Take a picture of your hands inside the rectangle")
+
+if picture:
+    st.image(picture)
+
